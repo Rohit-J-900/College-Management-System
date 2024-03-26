5d7785cb8c0b54885d99482ea97a2676f54a00b2
Rohit J <159022457+Rohit-J-900@users.noreply.github.com>
2024-03-27 00:35:43 +0530

    AboutManagement.php

--- AboutManagement.php
+++ AboutManagement.php
@@ -2,8 +2,8 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"/>
-        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/> 
-        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.css"/> 
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.css"/>
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css"/>
         <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
         <link rel="stylesheet" type="text/css" href="css/home.css" />
